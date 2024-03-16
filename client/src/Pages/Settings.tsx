22ac3f1855c6821be15ced85abde685d2f63f7a3
= <yeohchongyics@gmail.com>
2024-03-17 01:14:46 +0800

    Change Settings styling

--- client/src/Pages/Settings.tsx
+++ client/src/Pages/Settings.tsx
@@ -1,25 +1,74 @@
 import NotificationConfig from "../Interfaces/NotificationConfig";
 import { Position } from "../Types/Position";
+import "./Settings.css";
 
-const Settings = (props: { config: NotificationConfig } ) => {
+const Settings = (props: { config: NotificationConfig }) => {
     return (
-        <div>
-            <div>
-                Notification count: <input type="number" value={props.config.count} onChange={(e) => props.config.setCount(parseInt(e.target.value))} />
+        <div className="settings-div">
+            <div className="settings-container">
+                <span>Notification count</span>
+                <input
+                    type="number"
+                    value={props.config.count}
+                    onChange={(e) => props.config.setCount(parseInt(e.target.value))}
+                />
             </div>
-            <div>
-                Notification position: <select value={props.config.position} onChange={(e) => props.config.setPosition(e.target.value as Position)}>
-                    <option value="TOP_LEFT">Position 1</option>
-                    <option value="TOP_RIGHT">Position 2</option>
-                    <option value="BOTTOM_LEFT">Position 3</option>
-                    <option value="BOTTOM_RIGHT">Position 4</option>
-                </select>
+            <div className="settings-container">
+                <span>Notification position</span>
+                <div className="settings-radio-button-div">
+                    <label htmlFor="position1">Position 1</label>
+                    <input
+                        type="radio"
+                        id="position1"
+                        name="position"
+                        value="TOP_LEFT"
+                        checked={props.config.position === "TOP_LEFT"}
+                        onChange={(e) => props.config.setPosition(e.target.value as Position)}
+                    />
+
+                    <label htmlFor="position2">Position 2</label>
+                    <input
+                        type="radio"
+                        id="position2"
+                        name="position"
+                        value="TOP_RIGHT"
+                        checked={props.config.position === "TOP_RIGHT"}
+                        onChange={(e) => props.config.setPosition(e.target.value as Position)}
+                    />
+
+                    <label htmlFor="position3">Position 3</label>
+                    <input
+                        type="radio"
+                        id="position3"
+                        name="position"
+                        value="BOTTOM_LEFT"
+                        checked={props.config.position === "BOTTOM_LEFT"}
+                        onChange={(e) => props.config.setPosition(e.target.value as Position)}
+                    />
+
+                    <label htmlFor="position4">Position 4</label>
+                    <input
+                        type="radio"
+                        id="position4"
+                        name="position"
+                        value="BOTTOM_RIGHT"
+                        checked={props.config.position === "BOTTOM_RIGHT"}
+                        onChange={(e) => props.config.setPosition(e.target.value as Position)}
+                    />
+                </div>
             </div>
-            <div>
-                Notification disappear time: <input type="number" value={props.config.disappearTime} onChange={(e) => props.config.setDisappearTime(parseInt(e.target.value))} />
+            <div className="settings-container">
+                <span>Notification disappear time</span>
+                <input
+                    type="number"
+                    value={props.config.disappearTime}
+                    onChange={(e) => props.config.setDisappearTime(parseInt(e.target.value))}
+                />
+                <span>sec</span>
             </div>
         </div>
     );
 };
+
 
 export default Settings;
